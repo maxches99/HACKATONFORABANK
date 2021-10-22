c2ab3156feb8bfc3d1e3580b2c72ca7225f42689
Max Chesnikov <m.chesnikov@app-smart.ru>
2021-10-23 02:22:44 +0300

    Initial Commit

---
+++ HACKATONFORABANK/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HACKATONFORABANK
+//
+//  Created by Максим Чесников on 23.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HACKATONFORABANK/HACKATONFORABANKApp.swift
@@ -0,0 +1,17 @@
+//
+//  HACKATONFORABANKApp.swift
+//  HACKATONFORABANK
+//
+//  Created by Максим Чесников on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct HACKATONFORABANKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
